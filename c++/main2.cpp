1dacc063cbf556861095c017ff5385997d146500
Avoca9o <davladimirov@edu.hse.ru>
2023-11-19 15:07:59 +0300

    add main.sh and c++ programs

---
+++ c++/main2.cpp
@@ -0,0 +1,8 @@
+class Class {};
+
+int main() {
+    for (int i = 0; i < 1000000000; ++i) {
+	Class obj;
+    }
+    return 0;
+}
---
+++ c++/main3.cpp
@@ -0,0 +1,19 @@
+class Base {
+public:
+    virtual void do_nothing() const = 0;
+    virtual ~Base() {}
+};
+
+class Derived : public Base {
+    void do_nothing() const {}
+    ~Derived() {}
+};
+
+int main() {
+    Base* obj = new Derived();
+    for (int i = 0; i < 1000000000; ++i) {
+        obj->do_nothing();
+    }
+    delete obj;
+    return 0;
+}
